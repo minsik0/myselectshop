1b0f5c5da96f8d26ab193936e48bf85b4c79e29a
minsik0 <qwew1212@naver.com>
2025-09-17 16:27:05 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -38,9 +38,8 @@ public class ProductController {
             @RequestParam("size") int size,
             @RequestParam("sortBy") String sortBy,
             @RequestParam("isAsc")boolean isAsc,
-
-
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
         return productService.getProducts(userDetails.getUser(),
                 page-1,size, sortBy, isAsc
                 );
@@ -51,6 +50,20 @@ public class ProductController {
                           @RequestParam Long folderId,
                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
         productService.addFolder(productId, folderId, userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder(
+            @PathVariable Long folderId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc")boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+
+    ){
+        return productService.getProductsInFolder(folderId, page-1, size, sortBy, isAsc, userDetails.getUser());
+
     }
 
 }
